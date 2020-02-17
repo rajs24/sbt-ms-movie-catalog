c51f6b3e95969b43d244a04fed42bcf372e7486e
Arockiaraj Savarinathan <arockiarajs24@gmail.com>
2020-02-17 22:01:44 +0800

    initial project upload

---
+++ src/test/java/com/sbt/ms/movie/catalog/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbt.ms.movie.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
